02456bf0af006adf2f1ec23d594c5c46ebb5f740
dikeshchouhan <103301921+dikeshchouhan1@users.noreply.github.com>
2022-08-06 02:50:25 +0530

    Delete emailRE.py

--- emailRE.py
+++
@@ -1,9 +0,0 @@
-import email
-import re
-email_condition='^ [a-z]+[\._]?[a-z 0-9]+[@]\w +[.]\w{2,3}$ '
-user_email=input(' enter a email')
-
-if re.search(email_condition,user_email):
-    print(" right email ")
-else:
-    print(' wrong email')
